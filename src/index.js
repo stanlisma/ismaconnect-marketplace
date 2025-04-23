93829b36e3eed6eea96b1d791e396ba759a0376e
stanlisma <stanley.ismaillar@gmail.com>
2025-04-23 03:48:21 -0600

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+
+const App = () => <div>Welcome to IsmaConnect!</div>;
+
+ReactDOM.render(<App />, document.getElementById('root'));
